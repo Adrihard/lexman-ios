fa217f30b547fa92e5fcb9be0d665575fd83e3b5
Adrian Kalmar <adrian.kalmar@protonmail.com>
2018-12-16 20:38:42 +0100

    FEATURE: Re-used a ViewController to allow editing a lexicon (see #5).
    - Refactored some back-end static methods, and shortened the name of most of them.
    - Did some code cleanup.
    - Simplified the storyboard, by removing one of the navigation controllers.

--- lexman/LexiqueBDD.swift
+++ lexman/LexiqueBDD.swift
@@ -24,63 +24,44 @@ class LexiqueBDD {
         print(error);
     }
     
-    public static func createTableIfNotExists() {
-        do {
-            let createTable = table.create(ifNotExists: true) { table in
-                table.column(id, primaryKey: .autoincrement)
-                table.column(titre)
-                table.column(descriptif)
-            };
+    public static func createTableIfNotExists() throws {
+        let createTable = table.create(ifNotExists: true) { table in
+            table.column(id, primaryKey: .autoincrement)
+            table.column(titre)
+            table.column(descriptif)
+        };
             
-            try bdd.run(createTable);
-        }
+        try bdd.run(createTable);
+    }
+    
+    public static func selectAll() throws -> [Lexique] {
+        let cursor = try bdd.prepare(table);
         
-        catch {
-            errorHandling(error: error);
-        }
-    }
-    
-    public static func getTermesOfLexique(lexique: Lexique) {
-        LexiqueBDD.getTermesOfLexique(id_lexique: lexique.getId()!);
-    }
-    
-    public static func getTermesOfLexique(id_lexique: Int64) {
-        
-    }
-    
-    public static func selectAll() -> [Lexique] {
         var lexiques: [Lexique] = [];
         
-        do {
-            let cursor = try bdd.prepare(table);
-            
-            for row in cursor {
-                lexiques.append(
-                    Lexique(
-                        id: row[LexiqueBDD.id],
-                        titre: row[LexiqueBDD.titre],
-                        descriptif: row[LexiqueBDD.descriptif]
-                    )
-                );
-            }
-        }
-        catch {
-            errorHandling(error: error);
+        for row in cursor {
+            lexiques.append(
+                Lexique(
+                    id: row[LexiqueBDD.id],
+                    titre: row[LexiqueBDD.titre],
+                    descriptif: row[LexiqueBDD.descriptif]
+                )
+            );
         }
         
         return (lexiques);
     }
     
-    public static func insertLexique(lexique: Lexique) -> Bool {
+    public static func insert(lexique: Lexique) -> Bool {
         return (
-            LexiqueBDD.insertLexique(
+            LexiqueBDD.insert(
                 titre: lexique.getTitre(),
                 descriptif: lexique.getDescriptif()
             )
         );
     }
     
-    public static func insertLexique(titre: String, descriptif: String?) -> Bool {
+    public static func insert(titre: String, descriptif: String?) -> Bool {
         do {
             let insert = LexiqueBDD.table.insert(
                 LexiqueBDD.titre <- titre,
@@ -98,16 +79,15 @@ class LexiqueBDD {
         return (false);
     }
     
-    public static func deleteLexique(lexique: Lexique) -> Bool {
-        return (LexiqueBDD.deleteLexique(id: lexique.getId()));
-    }
-    
-    public static func deleteLexique(id: Int64?) -> Bool {
-        if (id != nil && id! >= 0) {
+    public static func update(id: Int64?, titre: String, descriptif: String?) -> Bool {
+        let lexique = LexiqueBDD.ligne(id: id);
+        
+        if (lexique != nil) {
             do {
-                let lexique = LexiqueBDD.table.filter(LexiqueBDD.id == id!);
-                
-                try LexiqueBDD.bdd.run(lexique.delete());
+                try LexiqueBDD.bdd.run(lexique!.update(
+                    LexiqueBDD.titre <- titre,
+                    LexiqueBDD.descriptif <- descriptif
+                ));
                 
                 return (true);
             }
@@ -119,36 +99,70 @@ class LexiqueBDD {
         return (false);
     }
     
-    public static func getTermesOfLexique(lexique: Lexique) -> [Terme] {
-        var termes: [Terme] = [];
+    public static func delete(lexique: Lexique) -> Bool {
+        return (LexiqueBDD.delete(id: lexique.getId()));
+    }
+    
+    public static func delete(id: Int64?) -> Bool {
+        let lexique = LexiqueBDD.ligne(id: id);
         
-        do {
-            let join = LexiqueBDD.table.join(
-                TermeBDD.table,
-                on: LexiqueBDD.id == TermeBDD.lexique
-            );
-            
-            let query = join.select(
-                TermeBDD.table[*]
-            )
-            
-            let cursor = try bdd.prepare(query);
-            
-            for row in cursor {
-                termes.append(
-                    Terme(
-                        id:         row[TermeBDD.id],
-                        titre:      row[TermeBDD.titre],
-                        descriptif: row[TermeBDD.descriptif]
-                    )
-                );
+        if (lexique != nil) {
+            do {
+                try LexiqueBDD.bdd.run(lexique!.delete());
+                return (true);
+            }
+            catch {
+                errorHandling(error: error);
             }
         }
         
-        catch {
-            errorHandling(error: error);
+        return (false);
+    }
+    
+    public static func exists(lexique: Lexique) throws -> Bool {
+        return (try LexiqueBDD.exists(id: lexique.getId()));
+    }
+    
+    public static func exists(id: Int64?) throws -> Bool {
+        let lexique = LexiqueBDD.ligne(id: id);
+        
+        if (lexique != nil) {
+            let count = try LexiqueBDD.bdd.scalar(lexique!.count);
+            return (count > 0);
+        }
+        
+        return (false);
+    }
+    
+    public static func getTermes(lexique: Lexique) throws -> [Terme] {
+        let join = LexiqueBDD.table.join(
+            TermeBDD.table,
+            on: LexiqueBDD.id == TermeBDD.lexique
+        );
+            
+        let query = join.select(
+            TermeBDD.table[*]
+        )
+        
+        let cursor = try bdd.prepare(query);
+        
+        var termes: [Terme] = [];
+        
+        for row in cursor {
+            termes.append(Terme(
+                id:         row[TermeBDD.id],
+                titre:      row[TermeBDD.titre],
+                descriptif: row[TermeBDD.descriptif]
+            ));
         }
         
         return (termes);
+    }
+
+    private static func ligne(id: Int64?) -> Table? {
+        if (id != nil && id! >= 0) {
+            return (LexiqueBDD.table.filter(LexiqueBDD.id == id!));
+        }
+        return (nil);
     }
 }
