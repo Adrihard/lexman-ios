05004f1b198e8229a26a8b120131c87bd2fa3f6e
Adrian Kalmar <adrian.kalmar@protonmail.com>
2018-12-18 21:31:11 +0100

    FEATURE: Added methods required for handling entries.

--- lexman/TermeBDD.swift
+++ lexman/TermeBDD.swift
@@ -42,4 +42,81 @@ class TermeBDD {
             errorHandling(error: error);
         }
     }
+    
+    public static func insert(terme: Terme, lexique: Lexique) -> Bool {
+        return (
+            TermeBDD.insert(
+                titre: terme.getTitre(),
+                descriptif: terme.getDescriptif(),
+                id_lexique: lexique.getId()
+            )
+        );
+    }
+    
+    public static func insert(titre: String, descriptif: String, id_lexique: Int64?) -> Bool {
+        if (id_lexique != nil && id_lexique! >= 0) {
+            do {
+                let insert = TermeBDD.table.insert(
+                    TermeBDD.titre <- titre,
+                    TermeBDD.descriptif <- descriptif,
+                    TermeBDD.lexique <- id_lexique!
+                );
+                
+                try bdd.run(insert);
+                return (true);
+            }
+                
+            catch {
+                errorHandling(error: error)
+            }
+        }
+        return (false);
+    }
+    
+    public static func update(id: Int64?, titre: String, descriptif: String) -> Bool {
+        let terme = TermeBDD.ligne(id: id);
+        
+        if (terme != nil) {
+            do {
+                try TermeBDD.bdd.run(terme!.update(
+                    TermeBDD.titre <- titre,
+                    TermeBDD.descriptif <- descriptif
+                ));
+                
+                return (true);
+            }
+            catch {
+                errorHandling(error: error);
+            }
+        }
+        
+        return (false);
+    }
+    
+    public static func delete(terme: Terme) -> Bool {
+        return (TermeBDD.delete(id: terme.getId()));
+    }
+    
+    public static func delete(id: Int64?) -> Bool {
+        let terme = TermeBDD.ligne(id: id);
+        
+        if (terme != nil) {
+            do {
+                try TermeBDD.bdd.run(terme!.delete());
+                return (true);
+            }
+            catch {
+                errorHandling(error: error);
+            }
+        }
+        
+        return (false);
+    }
+    
+    private static func ligne(id: Int64?) -> Table? {
+        if (id != nil && id! >= 0) {
+            return (TermeBDD.table.filter(TermeBDD.id == id!));
+        }
+        return (nil);
+    }
 }
